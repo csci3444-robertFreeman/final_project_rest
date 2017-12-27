b137532180430c04bde4e241bea06464bf96adae
RobertF00 <robertf@student.fdu.edu>
2017-12-27 01:30:29 -0500

    initial commit

---
+++ test/rest/controller/teacherRestControllerSpec.js
@@ -0,0 +1,330 @@
+var mongoose = require('mongoose');
+
+var TeacherRestController = function(TeacherModel) {
+    /**
+     * Fulfills GET on an  echo service to check REST service is UP
+     * It returns "echo REST GET returned input msg:" + req.params.msg
+     * http://localhost:9016/teachers/echo/:msg     GET
+     * http://localhost:9016/teachers/echo/hohoho   GET
+     * 
+       curl -i http://localhost:9016/teachers/echo/hohoho
+     * @param {*} req 
+     * @param {*} res 
+     */
+    var echoMsg = function(req, res) {
+        res.status(200);
+        res.send("echo REST GET returned input msg:" + req.params.msg); // NOTE ilker 'res.send("echo REST GET returned input msg:%s", req.params.msg)' is WRONG. Syntax is 'res.send(status, body)'
+    };
+
+    /**
+     * Fulfills GET REST requests. Returns collection of all teachers in mondodb using the TeacherModel of mongoose that was injected via constructor function
+     * http://localhost:9016/teachers            GET
+     * http://localhost:9016/api/v1/teachers     GET
+     * http://localhost:9016/api/v2/teachers     GET  - not implemented in app, just shown as example
+     * 
+       curl -i http://localhost:9016/teachers
+       curl -i http://localhost:9016/api/v1/teachers
+     * 
+     * @param {*} req Request
+     * @param {*} res Response
+     */
+    var find = function(req, res) {
+        TeacherModel.find(function(error, teachers) {
+            if (error) {
+                res.status(500);
+                res.send("Internal server error");
+            } else {
+                res.status(200);
+                res.send(teachers);
+            }
+        });
+    };
+
+    /**
+     * Fulfills GET for and id in url REST requests.
+     * It returns the teacher instance whose _id value is specified in url and passed as req.params._id
+     * You can find pick one of the row's _id value from mongodb, in its client mongo, from output of
+     * > db.teachers.find()
+     * http://localhost:9016/teachers/:id                                GET
+     * http://localhost:9016/teachers/5a1464bf3322b34128b20c8c           GET
+     * http://localhost:9016/api/v1/teachers/:id                         GET
+     * 
+       curl  http://localhost:9016/teachers/5a1464bf3322b34128b20c8c
+       curl -i http://localhost:9016/teachers/5a1464bf3322b34128b20c8c
+       curl -i -X GET http://localhost:9016/teachers/5a1464bf3322b34128b20c8c
+       curl  http://localhost:9016/api/v1/teachers/5a1464bf3322b34128b20c8c
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
+    var findById = function(req, res) {
+        if (req.params && req.params.id && mongoose.Types.ObjectId.isValid(req.params.id)) {
+            TeacherModel.findById(req.params.id, function(error, teacher) {
+                if (error) {
+                    res.status(404); // 404 means not found
+                    res.send("Not found Teacher for id:" + req.params.id);
+                } else {
+                    res.status(200);
+                    res.send(teacher);
+                }
+            });
+        } else {
+            res.status(400); // 400 means "Bad Request" (incorrect input)
+            res.send("Check inputs of request. InCorrect inputs. Expected _id value in url of GET request. req.params.id:" + req.params.id);
+        }
+    };
+
+    /**
+     * Fulfills POST REST requests. Directly saves teacher object that was passed in req.body.
+     * NOTE _id (the primary key that mongodb by default creates an index to speed up finds on) and __v (meaning version) will be added to record by mongodb itself
+     * http://localhost:9016/teachers             POST
+     * 
+        curl -X POST -H "Content-Type: application/json" -i -d  '{"teacherId": 0, "name":"Rob", "lastname":"Freeman", "salary": 12, "age":22, "isFullTime":false}' http://localhost:9016/teachers
+        curl -X POST -H "Content-Type: application/json" -i -d  '{"teacherId": 1, "name":"Paul", "lastname":"Jerchaflie", "salary": 10, "age":21, "isFullTime":false}' http://localhost:9016/teachers
+        curl -X POST -H "Content-Type: application/json" -i -d  '{"teacherId": 2, "name":"Matt", "lastname":"Jacobs", "salary": 20, "age":23, "isFullTime":false}' http://localhost:9016/teachers
+        curl -X POST -H "Content-Type: application/json" -i -d  '{"teacherId": 3, "name":"Peter", "lastname":"Jerchaflie", "salary": 12, "age":21, "isFullTime":true}' http://localhost:9016/teachers
+        curl -X POST -H "Content-Type: application/json" -i -d  '{"teacherId": 4, "name":"Rich", "lastname":"Ramos", "salary": 15, "age":22, "isFullTime":true}' http://localhost:9016/teachers
+* 
+     * In postman, select POST as method, click Body, click raw, select "JSON(application/json)" pulldown, enter below
+     * {
+     *   "teacherId": 4,
+     *   "name": "ilker_4 ",
+     *   "lastname": "kiris_4",
+     *   "salary": "FreshMan ",
+     *   "age": 204,
+     *   "isFullTime": false
+     * }
+     * @param {*} request 
+     * @param {*} response 
+     */
+    var save = function(request, response) {
+        var teacher = new TeacherModel(request.body);
+        console.log("--> LOOK request: %s", request); // JSON.stringify(request)
+        console.log("--> LOOK JSON.stringify(request.body): %s", JSON.stringify(request.body));
+        console.log("--> LOOK request.body: %s", request.body);
+        console.log("--> LOOK teacher: %s", teacher);
+        teacher.save(function(error) {
+            if (error) {
+                response.status(500);
+                response.send("Save failed");
+            } else {
+                response.status(201); // 201 means created
+                response.send(teacher);
+            }
+        });
+    };
+
+    // id: Number
+    // name: String,
+    // lastname: String,
+    // salary: String,
+    // age: Number,
+    // isFullTime: { type: Boolean, default: true }
+    // updatedOn: Date
+
+    /**
+     * Fulfills PUT REST requests. This is NOT partial update, but full update of teacher record in mongodb for id in the url 
+     * 1) Find the teacher from mongodb by id provided in the url
+     * 2) Set teacher fetched from mongodb to have values of all the attributes expected to be in the body of request
+     * 3) Save the replaced teacher back to mongodb
+     * http://localhost:9016/teachers/:id                       PUT
+     * http://localhost:9016/teachers/5a23e035decd2b6770ab4890  PUT
+     * 
+       curl -X PUT -H "Content-Type: application/json" -i -d '{"teacherId": 0, "name":"Rob_0_update_1",   "lastname":"freeman_0", "salary":43, "age":22, "isFullTime":true}' http://localhost:9016/teachers/5a385cbd235e032f24c94eef
+       curl -X PUT -H "Content-Type: application/json" -i -d '{"teacherId": 1, "name":"Paul_1_update_1", "lastname":"jerchaflie_1", "salary": 20, "age": 21, "isFullTime":false, "updatedOn":"2017-12-03T12:39:06.446Z"}' http://localhost:9016/teachers/5a23f72a1fb00a38f0a814a9
+       curl -X PUT -H "Content-Type: application/json" -i -d '{"teacherId": 2, "name":"Matt_2_update_1", "lastname":"jacobs_2", "salary":15, "age":23, "isFullTime":true, "updatedOn":"'"$(date +%Y-%m-%dT%H:%M:%S)"'"}' http://localhost:9016/teachers/5a23f72a1fb00a38f0a814a9
+       curl -X PUT -H "Content-Type: application/json" -i -d '{"teacherId": 3, "name":"Peter_3_update_1",   "lastname":"jerchaflie_3", "salary":32, "age":21, "isFullTime":false, "updatedOn":"'"$(date +%Y-%m-%dT%H:%M:%S)"'"}' http://localhost:9016/teachers/5a2f2505c96a552334fdc762
+       curl -X PUT -H "Content-Type: application/json" -i -d '{"teacherId": 3, "name":"Rich_3_update_1",   "lastname":"ramos_4", "salary":50, "age":22, "isFullTime":false, "updatedOn":"'"$(date +%Y-%m-%dT%H:%M:%S)"'"}' http://localhost:9016/teachers/5a2f2505c96a552334fdc762
+     * 
+     * In postman, select PUT as method, click Body, click raw, select "JSON(application/json)" pulldown,
+     * in url enter   http://localhost:9016/teachers/5a23e72b0a47f03f787cd618
+     * in "Pre-request Script" tab of postman, enter
+     *   postman.setGlobalVariable("myCurrentDate", new Date().toISOString());
+     * in "Body" tab of postman, enter
+     * {
+     *   "teacherId": 4,
+     *   "name": "ilker_4_update",
+     *   "lastname": "kiris_4",
+     *   "salary": "FreshMan ",
+     *   "age": 204,
+     *   "isFullTime": false,
+     *   "updatedOn": "{{myCurrentDate}}"
+     * }
+     * @param {*} req 
+     * @param {*} res 
+     */
+    var findByIdUpdateFullyThenSave = function(req, res) {
+        if (req.params && req.params.id && mongoose.Types.ObjectId.isValid(req.params.id)) {
+            TeacherModel.findById(req.params.id, function(error, teacher) {
+                if (error) {
+                    res.status(404); // 404 means not found
+                    res.send("Not found Teacher for id:" + req.params.id);
+                } else {
+                    console.log("req.body.updatedOn: %s", req.body.updatedOn);
+                    teacher.teacherId = req.body.teacherId;
+                    teacher.name = req.body.name;
+                    teacher.lastname = req.body.lastname;
+                    teacher.salary = req.body.salary;
+                    teacher.age = req.body.age;
+                    teacher.isFullTime = req.body.isFullTime;
+                    teacher.updatedOn = req.body.updatedOn;
+
+                    teacher.save(function(error) {
+                        if (error) {
+                            res.status(500);
+                            res.send("Save failed");
+                        } else {
+                            res.status(201); // 201 means created
+                            res.send(teacher);
+                        }
+                    });
+                }
+            });
+        } else {
+            res.status(400); // 400 means "Bad Request" (incorrect input)
+            res.send("Check inputs of request. InCorrect inputs. Expected _id value in url of PUT request. req.params.id:" + req.params.id);
+        }
+    };
+
+    /**
+     * Fulfills PATCH REST requests. NOTE this is allows partial update of teacher record. 
+     * 1) Find the teacher from mongodb by id provided in the url
+     * 2) Loop over the attribute names in the body of request and set their values in the teacher that was fetched from mongodb
+     * 3) Save the updated teacher back to mongodb
+     * http://localhost:9016/teachers/:id             PATCH
+     * 
+       curl -X PATCH -H "Content-Type: application/json" -i -d '{"salary":"sophomore", "age":123, "isFullTime":false}' http://localhost:9016/teachers/5a2634cd4d7bbb2edcfef2da
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
+    var findByIdUpdatePartiallyThenSave = function(req, res) {
+        if (req.params && req.params.id && mongoose.Types.ObjectId.isValid(req.params.id)) {
+            TeacherModel.findById(req.params.id, function(error, teacher) {
+                if (error) {
+                    res.status(404); // 404 means not found
+                    res.send("Not found Teacher for id:" + req.params.id);
+                } else {
+                    // if incoming PUT request's body has accidentally _id, remove it from req.body
+                    if (req.body._id) {
+                        delete req.body._id;
+                    }
+                    // loop over the attributes in req.body and set them in teacher object
+                    for (var attrName in req.body) {
+                        teacher[attrName] = req.body[attrName];
+                    }
+
+                    teacher.save(function(error) {
+                        if (error) {
+                            res.status(500);
+                            res.send("Save failed");
+                        } else {
+                            res.status(201); // 201 means created - in this case means updated
+                            res.send(teacher);
+                        }
+                    })
+                }
+            });
+        } else {
+            res.status(400); // 400 means "Bad Request" (incorrect input)
+            res.send("Check inputs of request. InCorrect inputs. Expected _id value in url of PATCH request. req.params.id:" + req.params.id);
+        }
+    };
+
+    /**
+     * Fulfills DELETE REST requests.
+     * Removes(Deletes) the teacher row whose _id value is specied in the url and passed in req.
+     * NOTE _id and _version will be added to record by mongodb itself
+     * http://localhost:9016/teachers/:id             DELETE
+     * 
+       curl -X DELETE -i http://localhost:9016/teachers/5a23f72a1fb00a38f0a814a9
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
+    var findByIdThenRemove = function(req, res) {
+        try {
+            console.log("findByIdThenRemove req.params.id:%s", req.params.id);
+            // NOTE ilker mongoose.Types.ObjectId.isValid(req.params.id) returns true for any 12 byte string input
+            if (req.params && req.params.id && mongoose.Types.ObjectId.isValid(req.params.id)) {
+                // if (req.params && req.params.id) {
+                console.log(" again findByIdThenRemove req.params.id:%s", req.params.id);
+                TeacherModel.findById(req.params.id, function(error, teacher) {
+                    if (error) {
+                        console.log("findByIdThenRemove error:" + error);
+                        res.status(404); // 404 means not found
+                        res.send("Not found Teacher for id:" + req.params.id);
+                    } else {
+                        teacher.remove(function(error) {
+                            if (error) {
+                                res.status(500);
+                                res.send("Remove failed");
+                            } else {
+                                res.status(204); // 204 means deleted
+                                res.send(teacher);
+                            }
+                        })
+                    }
+                });
+            } else {
+                res.status(400); // 400 means "Bad Request" (incorrect input)
+                res.send("Check inputs of request. InCorrect inputs. Expected _id value in url of DELETE request. req.params.id:" + req.params.id);
+            }
+
+        } catch (e) {
+            res.status(500); // 500 means "Internal Server Error". could also be due to mongodb/js-bson#205 bug that throws CastError, not being able to parse the wrong(short) _id value to objectId
+            res.send("Check inputs of request. InCorrect inputs. Expected _id value in url of DELETE request may be not a valid ObjectId value. req.params.id:" + req.params.id);
+        }
+    };
+
+    /**
+     * Fulfills DELETE REST requests.
+     * Removes(Deletes) the teacher row whose _id value is specied in the body and passed in req.body._id
+     * http://localhost:9016/teachers             DELETE
+     * 
+       curl -X DELETE -H "Content-Type: application/json" -i -d '{"_id":"5a2f1ef568f053451051ebdb"}' http://localhost:9016/teachers
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
+    var findByIdInBodyThenRemove = function(req, res) {
+        console.log("findByIdInBodyThenRemove req.body._id:%s", req.body._id);
+        if (req.body && req.body._id && mongoose.Types.ObjectId.isValid(req.body._id)) {
+            TeacherModel.findById(req.body._id, function(error, teacher) {
+                if (error) {
+                    res.status(404); // 404 means "not found""
+                    res.send("Not found Teacher for id:" + req.body._id);
+                } else {
+                    console.log("LAGA%sLUGA", error);
+                    teacher.remove(function(error) {
+                        if (error) {
+                            res.status(500);
+                            res.send("Remove failed");
+                        } else {
+                            res.status(204); // 204 means deleted ("No Content")
+                            res.send(teacher);
+                        }
+                    })
+                }
+            });
+
+        } else {
+            res.status(400); // 400 means "Bad Request" (incorrect input)
+            res.send("Check inputs of request. InCorrect inputs. Expected _id in body of DELETE request");
+        }
+    };
+
+    // expose public functions via returned object below from this module
+    return {
+        echoMsg: echoMsg,
+        find: find,
+        findById: findById,
+        save: save,
+        findByIdUpdateFullyThenSave: findByIdUpdateFullyThenSave,
+        findByIdUpdatePartiallyThenSave: findByIdUpdatePartiallyThenSave,
+        findByIdThenRemove: findByIdThenRemove,
+        findByIdInBodyThenRemove: findByIdInBodyThenRemove
+    }
+};
+
+module.exports = TeacherRestController;
